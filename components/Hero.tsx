3f4d271e0d5be994227a9ca0e34649983619e744
Moussa <mosisg7@gmail.com>
2024-10-27 12:52:46 +0100

    Description des modifications

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
-import { FaUsers, FaRegLightbulb } from 'react-icons/fa'; // Icônes modernisées
+import { FaUsers, FaRegLightbulb } from 'react-icons/fa'; 
+import '@/app/globals.css'
 
 const Hero: React.FC = () => {
     return (
-        <div className="flex flex-col items-center justify-center h-[60vh] bg-gradient-to-b from-[#e0dbd4] to-[#ffffff] p-4">
+        <div className="flex flex-col items-center justify-center h-[60vh] bg-gradient-to-b from-[#e0dbd4] to-[#ffffff] pt-24 md:py-36">
             <h1 className="text-4xl md:text-6xl font-bold mb-8 text-center text-[#333] transition transform duration-500 hover:scale-105">
-                In verbinding met jezelf, <br />in verbinding met je gezin
+                In verbinding met jezelf, <br />
+                <span className="text-animation">in verbinding met je gezin</span>
+
             </h1>
             <div className="flex flex-col md:flex-row justify-around w-full max-w-4xl px-4">
                 <div className="flex flex-col items-center transition-transform duration-300 hover:scale-105 mb-6 md:mb-0">
@@ -24,7 +27,7 @@ const Hero: React.FC = () => {
                 href="#contact"
                 className="mt-6 px-6 py-3 text-white bg-[#0065de] rounded-lg shadow-lg hover:bg-[#0056b3] transition duration-300"
             >
-                Contactez-moi
+                Contacten
             </a>
         </div>
     );
